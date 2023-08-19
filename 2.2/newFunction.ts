ab54a71852d434a7284d866a07f7bcb65a8e3f1e
Shawn Rowley <shawnrowley@Shawn-Rowley-C02HD10CQ05N.local>
2023-08-19 10:37:09 -0600

    testing single quotes and adding a comment!

--- 2.2/newFunction.ts
+++ 2.2/newFunction.ts
@@ -1,11 +1,17 @@
+// with the function 'myFunction' the parameter 'myNum' should always be provided with a number as an argument, and the returned output is expected to be a string
 export function myFunction(myNum: number): string {
+    //if the number is exactly 5 the return the word 'true'
     if (myNum == 5) {
         return 'true'
-    } else if(myNum > 5) {
+    // if the number provided is less then 5 but not a negative number return 'false'
+    } else if(myNum < 5 && myNum > 0) {
         return 'false'
-    }else if (){
-
+    // if the number is greater then 5 return 'big'
+    }else if (myNum > 5){
+        return 'big'
+    // if not any of the above (a negative number) return 'negative'
     }else {
+        return 'negative'
         
     }
 }
